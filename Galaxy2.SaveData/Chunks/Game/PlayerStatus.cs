e12893d5470f947f5f20ec9791764be99f466e88
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-11-27 04:18:36 +0100

    Add debug code

--- Galaxy2.SaveData/Chunks/Game/PlayerStatus.cs
+++ Galaxy2.SaveData/Chunks/Game/PlayerStatus.cs
@@ -23,7 +23,52 @@ public class SaveDataStoragePlayerStatus
 
         var (attributes, headerDataSize) = reader.ReadAttributesAsDictionary();
         var fieldsDataStartPos = reader.BaseStream.Position;
+        
+        foreach (var (k,o) in attributes)
+        {
 
+            reader.BaseStream.Position = fieldsDataStartPos + o;
+            var u8 = reader.ReadByte();
+            reader.BaseStream.Position = fieldsDataStartPos + o;
+            var u16 = reader.ReadUInt16();
+                
+            Console.WriteLine($"Attribute key: 0x{k:X4}, offset: 0x{o:X4} => u8: {u8}, u16: {u16}");
+        }
+        
+        // TODO attributes 
+        /* WII
+         
+         Attribute key: 0x4ED5, offset: 0x0000 => u8: 9, u16: 2304
+           Attribute key: 0xE352, offset: 0x0001 => u8: 0, u16: 178
+           Attribute key: 0x450D, offset: 0x0003 => u8: 1, u16: 328
+           Attribute key: 0x23EC, offset: 0x0005 => u8: 1, u16: 300
+           Attribute key: 0x7579, offset: 0x0007 => u8: 0, u16: 70
+           
+         */
+        
+        /* SWITCH
+         
+         Attribute key: 0x4ED5, offset: 0x0000 => u8: 22, u16: 9750
+           Attribute key: 0xE352, offset: 0x0001 => u8: 38, u16: 38
+           Attribute key: 0x450D, offset: 0x0003 => u8: 88, u16: 88
+           Attribute key: 0x23EC, offset: 0x0005 => u8: 0, u16: 0
+           Attribute key: 0x7579, offset: 0x0007 => u8: 0, u16: 512
+           
+           Attribute key: 0xAA83, offset: 0x0008 => u8: 2, u16: 2
+           Attribute key: 0x7213, offset: 0x0009 => u8: 0, u16: 0
+           Attribute key: 0xEA99, offset: 0x000A => u8: 0, u16: 0
+           Attribute key: 0xBF77, offset: 0x000B => u8: 0, u16: 0
+           Attribute key: 0xBFD3, offset: 0x000D => u8: 32, u16: 32
+           Attribute key: 0x1E85, offset: 0x000F => u8: 3, u16: 3
+           Attribute key: 0xEFDB, offset: 0x0011 => u8: 0, u16: 0
+           Attribute key: 0xE6D1, offset: 0x0013 => u8: 0, u16: 0
+           Attribute key: 0x3D5F, offset: 0x0015 => u8: 73, u16: 1609
+           Attribute key: 0x0AC6, offset: 0x0019 => u8: 64, u16: 64
+           Attribute key: 0x71E3, offset: 0x001D => u8: 0, u16: 0
+           Attribute key: 0xE983, offset: 0x0021 => u8: 0, u16: 0
+                    
+         */
+        
         if (reader.TryReadU8(fieldsDataStartPos, attributes, "mPlayerLeft", out var playerLeft))
             status.PlayerLeft = playerLeft;
         if (reader.TryReadU16(fieldsDataStartPos, attributes, "mStockedStarPieceNum", out var sspn))
